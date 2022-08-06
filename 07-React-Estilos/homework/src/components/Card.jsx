75a377c33f31b14b895bd4be8621823683589b30
Carsefer <carlosm.rodriguez4@gmail.com>
2022-08-06 13:54:38 -0500

    terminado

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import a from "./Card.module.css";
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
-};
+  return (
+    <div className={a.cuadro}>
+      <button className={a.btn} onClick={props.onClose}>
+        X
+      </button>
+      <h4>{props.name}</h4>
+      <div className={a.infoCuadro}>
+        <div>
+          <h5>Min</h5>
+          <p>{props.min}</p>
+        </div>
+        <div>
+          <h5>Max</h5>
+          <p>{props.max}</p>
+        </div>
+        <div>
+          <img
+            src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}
+            alt="Not Found"
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
+}
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import X from "./SearchBar.module.css";
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
-};
+  return (
+    <div className={X.cuadro}>
+      <input type="text" placeholder="Cities..." required />
+      <button onClick={() => props.onSearch("Estamos buscando")}>Add</button>
+    </div>
+  );
+}
